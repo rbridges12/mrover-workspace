c4e4b557d30e252762eec5a60b6f23025afd2be4
Rushil Talla <rushilt@umich.edu>
2022-01-09 14:03:52 -0500

    With calibrate

--- scripts/odrive_testing_script.py
+++ scripts/odrive_testing_script.py
@@ -3,6 +3,7 @@ from odrive.enums import AXIS_STATE_CLOSED_LOOP_CONTROL, CTRL_MODE_VELOCITY_CONT
 from odrive.utils import dump_errors
 import sys
 import select
+import fibre
 
 id = "205F3883304E"
     
@@ -18,15 +19,31 @@ def drive():
 
 def stop():
     global test_motor
+    test_motor.controller.vel_setpoint = 0
     test_motor.controller.current_setpoint = 0
     test_motor.requested_state = AXIS_STATE_IDLE
 
-def main():
+def main():``
     global odrv
     global test_motor
     odrv = odrive.find_any(serial_number=id)
     print("found odrive")
     test_motor = odrv.axis0
+
+    # Calibrate first
+    try:
+        test_motor.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+        test_motor.motor.config.pre_calibrated = True
+        test_motor.encoder.config.pre_calibrated = True
+        while(test_motor.requested_state != AXIS_STATE_IDLE):
+            pass
+        odrv.save_configuration()
+        odrv.reboot()
+    except fibre.protocol.ChannelBrokenException:
+        odrv = odrive.find_any(serial_number=id)
+        test_motor = odrv.axis0
+        print("found odrive")
+        pass
 
     while(1):
         cmd_input = select.select([sys.stdin], [], [], 1)[0]
