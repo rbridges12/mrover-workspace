56af92a5c1d2dd6f77391de8f2c30234e0e89b01
Riley Bridges <rlybrdgs@gmail.com>
2022-01-23 23:16:40 -0500

    added nav state controller and LCM message folder

---
+++ jetson/filter/test/nav_state_controller.py
@@ -0,0 +1,48 @@
+from pynput import keyboard
+# from rover_common import aiolcm
+import lcm
+from rover_msgs import NavStatus
+
+
+def send_nav_state(lc, state_name):
+    nav_struct = NavStatus()
+    nav_struct.nav_state_name = state_name
+
+    # these aren't used by the filter so just make them 0
+    nav_struct.completed_wps = 0
+    nav_struct.total_wps = 0
+
+    lc.publish("/nav_status", nav_struct.encode())
+
+def on_press(key):
+    # use nav state "Off" to stop the filter
+    try:
+        if key.char == 's':
+            send_nav_state(lc, "Off")
+            print("sent nav state \"Off\"")
+
+        # use nav state "Drive" to run the filter
+        elif key.char == 'd':
+            send_nav_state(lc, "Drive")
+            print("sent nav state \"Drive\"")
+            
+        elif key == keyboard.key.esc:
+            # keyboard.listener.stop()
+            return False 
+
+    except AttributeError:
+        pass
+    
+
+if __name__ == "__main__":
+
+    # init LCM
+    # lc = aiolcm.AsyncLCM()
+    lc = lcm.LCM()
+
+    # key listener loop
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+        
+            
