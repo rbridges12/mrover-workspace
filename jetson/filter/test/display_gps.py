48a5305e44b571950aef199f82e618be856cfbc7
Riley Bridges <rlybrdgs@gmail.com>
2022-02-09 00:59:38 -0500

    improved KML converter, added Alan's new data

--- jetson/filter/test/display_gps.py
+++ jetson/filter/test/display_gps.py
@@ -1,14 +1,17 @@
-from ast import parse
 import json
 import sys
-import simplekml
 
-EARTH_RADIUS_M = 6371000
+KML_HEADER = """<?xml version="1.0" encoding="UTF-8"?>
+                    <kml xmlns="http://www.opengis.net/kml/2.2">
+                        <Document id="1">"""
+KML_FOOTER = """        </Document>
+                    </kml>"""
+
 
 def parse_gps_data(gps_filename):
     with open(gps_filename, "r") as f:
         raw_msgs = f.read().split("\n\n")
-        
+
         msgs = []
         for raw_msg in raw_msgs:
 
@@ -17,18 +20,31 @@ def parse_gps_data(gps_filename):
 
             # replace single quotes with double quotes for JSON
             raw_msg = raw_msg.replace("\'", "\"")
-            
+
             if raw_msg != "":
                 msgs.append(json.loads(raw_msg))
     return msgs
 
+
+def new_placemark(lon, lat, name=0, id=0):
+    return f"""<Placemark id="{id}">
+                  <name>{name}</name>
+                  <Point id="{id}">
+                    <coordinates>{lon},{lat},0.0</coordinates>
+                  </Point>
+               </Placemark>"""
+
+
 def export_kml(gps_json, kml_filename):
-    kml = simplekml.Kml()
-    for point in gps_json:
-        lat_decimal = point["latitude_deg"] + point["latitude_min"] / 60
-        lon_decimal = point["longitude_deg"] + point["longitude_min"] / 60
-        kml.newpoint(name=point["timeStamp"], coords=[(lon_decimal, lat_decimal)])
-        kml.save(kml_filename)
+    placemarks = []
+    for i, point in enumerate(gps_json):
+        lon = point["longitude_deg"] + point["longitude_min"] / 60
+        lat = point["latitude_deg"] + point["latitude_min"] / 60
+        placemarks.append(new_placemark(lon, lat, i, i))
+
+    with open(kml_filename, 'w') as f:
+        f.write(KML_HEADER + "\n".join(placemarks) + KML_FOOTER)
+
 
 gps_file = "data/" + sys.argv[1]
 kml_file = "data/" + sys.argv[1].replace(".txt", ".kml")
